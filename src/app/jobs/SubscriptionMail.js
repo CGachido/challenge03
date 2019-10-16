eb5d80faec7922197c00fdfafbe282b05a71b084
Cassius Souza <cassius.sanches@hotmail.com>
2019-10-15 21:46:29 -0300

    Creating subscription mail and layout

---
+++ src/app/jobs/SubscriptionMail.js
@@ -0,0 +1,33 @@
+import { format, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import Mail from '../../lib/Mail';
+
+class SubscriptionMail {
+  get key() {
+    return 'SubscriptionMail';
+  }
+
+  async handle({ data }) {
+    const { meetup, user } = data;
+
+    await Mail.sendMail({
+      to: `${meetup.organizer.name} <${meetup.organizer.email}>`,
+      subject: 'New Subscription',
+      template: 'subscription',
+      context: {
+        organizer: meetup.organizer.name,
+        registered: user.name,
+        title: meetup.title,
+        date: format(
+          parseISO(meetup.date),
+          "'dia' dd 'de' MMMM', às' H:mm'h'",
+          {
+            locale: pt,
+          }
+        ),
+      },
+    });
+  }
+}
+
+export default new SubscriptionMail();
